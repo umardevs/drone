e16a6fa32494d5f446aa0d32b4e4bdecd795030c
umardevs <umartojgonbaev2@gmail.com>
2023-11-27 12:32:22 +0600

    drone

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
